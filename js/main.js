1f564390a8cc49c21e9333cbae3a968dcaa08457
EvgeniaVishnevskaya <55274706+EvgeniaVishnevskaya@users.noreply.github.com>
2020-03-05 20:43:03 +0300

    yt dsikj

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-let controller = new ScrollMagic.Controller()
-
-let main = document.getElementById('main')
-let icon = document.getElementById('icon')
-
-let box2Anim = anime({
-  targets: main,
-  opasity: 0,
-  autoplay: false,
-  duration: 1400
-})
-new ScrollMagic.Scene({
-  triggerElement: icon,
-
-})
-.addTo(controller)
-.on('enter', function () {
-  if(!box2Anim.completed){
-    box2Anim.play()
-  }
-
-})
+var bg = document.getElementById('main-2020');
+document.getElementById('main-2020-min').addEventListener("mouseover", function(){bg.style.opasity = 1;this.addEventListener("mouseout", function(){bg.style.background = "";});
+});
