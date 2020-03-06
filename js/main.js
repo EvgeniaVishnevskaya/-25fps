5acd5a26f7bcf22860f23fd288660a411e36dc35
EvgeniaVishnevskaya <55274706+EvgeniaVishnevskaya@users.noreply.github.com>
2020-03-06 23:53:02 +0300

    do

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-var bg = document.getElementById('main-2020');
+let bg = document.getElementById('main-2020');
+let op = document.getElementById('opasity');
 document.getElementById('main-2020-min').addEventListener("mouseover", function(){
+  console.log(123);
   bg.style.opacity = 1;
-  this.addEventListener("mouseout", function(){bg.style.background = "";
+  this.addEventListener("mouseout", function(){
+    bg.style.opacity = 0;
+  });
+});
+document.getElementById('do').addEventListener("mouseover", function(){
+  console.log(455);
+  bg.style.opacity = 1;
+  this.addEventListener("mouseout", function(){
+    bg.style.opacity = 0;
   });
 });
