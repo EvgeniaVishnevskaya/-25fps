eb13820f6c314608d5ad2386ddc333ac56a53e84
Evgenia <jeinifer.vishnevskaya@gmail.com>
2020-03-06 13:20:23 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 var bg = document.getElementById('main-2020');
-document.getElementById('main-2020-min').addEventListener("mouseover", function(){bg.style.opasity = 1;this.addEventListener("mouseout", function(){bg.style.background = "";});
+document.getElementById('main-2020-min').addEventListener("mouseover", function(){
+  bg.style.opacity = 1;
+  this.addEventListener("mouseout", function(){bg.style.background = "";
+  });
 });
