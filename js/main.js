a4e21528f7dfb00f14f2fb29f1451c6d50776999
EvgeniaVishnevskaya <55274706+EvgeniaVishnevskaya@users.noreply.github.com>
2020-03-07 16:57:34 +0300

    вроде все

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var scene = new ScrollMagic.Scene({
             triggerElement: "#se"
           })
           .setTween("#main", 0.5, {opacity: 0}) // trigger a TweenMax.to tween
-          .addIndicators({name: "1 (duration: 400)"}) // add indicators (requires plugin)
+          .addIndicators({name: "1 (duration: 1400)"}) // add indicators (requires plugin)
           .addTo(controller);
 
 
@@ -13,7 +13,7 @@ var scene2 = new ScrollMagic.Scene({
   triggerElement: "#se"
 })
   .setTween("#main-min", 0.5, {opacity: 0}) // trigger a TweenMax.to tween
-  .addIndicators({name: "1 (duration: 400)"}) // add indicators (requires plugin)
+  .addIndicators({name400)"}) // add indicators (requires plugin)
   .addTo(controller);
 
 let bg = document.getElementById('main-2020');
@@ -21,42 +21,42 @@ let sh = document.getElementById('main-showreel2019');
 let dm = document.getElementById('main-deepmerchlab2019');
 
 document.getElementById('main-2020-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   bg.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     bg.style.opacity = 0;
   });
 });
 document.getElementById('do').addEventListener("mouseover", function(){
-  console.log(455);
+  
   bg.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     bg.style.opacity = 0;
   });
 });
 document.getElementById('showreel2019').addEventListener("mouseover", function(){
-  console.log(123);
+  
   sh.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     sh.style.opacity = 0;
   });
 });
 document.getElementById('main-showreel2019-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   sh.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     sh.style.opacity = 0;
   });
 });
 document.getElementById('main-deepmerchlab2019-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   dm.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     dm.style.opacity = 0;
   });
 });
 document.getElementById('deepmerchlab2019').addEventListener("mouseover", function(){
-  console.log(123);
+  
   dm.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     dm.style.opacity = 0;
@@ -66,14 +66,14 @@ document.getElementById('deepmerchlab2019').addEventListener("mouseover", functi
 let tn = document.getElementById('main-tehnonikol2019');
 
 document.getElementById('main-tehnonikol2019-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   tn.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     tn.style.opacity = 0;
   });
 });
 document.getElementById('tehnonikol2019').addEventListener("mouseover", function(){
-  console.log(123);
+  
   tn.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     tn.style.opacity = 0;
@@ -84,14 +84,14 @@ document.getElementById('tehnonikol2019').addEventListener("mouseover", function
 let m9 = document.getElementById('main-moek2019');
 
 document.getElementById('main-moek2019-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   m9.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     m9.style.opacity = 0;
   });
 });
 document.getElementById('moek2019').addEventListener("mouseover", function(){
-  console.log(123);
+  
   m9.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     m9.style.opacity = 0;
@@ -101,14 +101,14 @@ document.getElementById('moek2019').addEventListener("mouseover", function(){
 let km = document.getElementById('main-komersant2019');
 
 document.getElementById('main-komersant2019-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   km.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     km.style.opacity = 0;
   });
 });
 document.getElementById('komersant2019').addEventListener("mouseover", function(){
-  console.log(123);
+  
   km.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     km.style.opacity = 0;
@@ -119,14 +119,14 @@ document.getElementById('komersant2019').addEventListener("mouseover", function(
 let dsh = document.getElementById('main-designshowreel2018');
 
 document.getElementById('main-designshowreel2018-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   dsh.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     dsh.style.opacity = 0;
   });
 });
 document.getElementById('designshowreel2018').addEventListener("mouseover", function(){
-  console.log(123);
+  
   dsh.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     dsh.style.opacity = 0;
@@ -136,14 +136,14 @@ document.getElementById('designshowreel2018').addEventListener("mouseover", func
 let fl = document.getElementById('main-flat2018');
 
 document.getElementById('main-flat2018-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   fl.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     fl.style.opacity = 0;
   });
 });
 document.getElementById('flat2018').addEventListener("mouseover", function(){
-  console.log(123);
+  
   fl.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     fl.style.opacity = 0;
@@ -153,14 +153,14 @@ document.getElementById('flat2018').addEventListener("mouseover", function(){
 let m8 = document.getElementById('main-moek2018');
 
 document.getElementById('main-moek2018-min').addEventListener("mouseover", function(){
-  console.log(123);
+  
   m8.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     m8.style.opacity = 0;
   });
 });
 document.getElementById('moek2018').addEventListener("mouseover", function(){
-  console.log(123);
+  
   m8.style.opacity = 1;
   this.addEventListener("mouseout", function(){
     m8.style.opacity = 0;
